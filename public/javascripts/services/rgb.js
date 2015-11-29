0c007b3a6cdf6f8f3528a49ab31a16ae892aebe7
Dimeji Abidoye <dimejy2@gmail.com>
2015-11-29 18:58:34 -0500

    working

---
+++ public/javascripts/services/rgb.js
@@ -0,0 +1,16 @@
+angular.module('myApp.services', [])
+  .factory('colorService', [ function() {
+
+    //var doRequest = function(username, path) {
+    //  return $http({
+   //     method: 'JSONP',
+   //     url: 'https://api.github.com/users/' + username + '/' + path + '?callback=JSON_CALLBACK'
+    //  });
+   // }
+   // return {
+   //   events: function(username) { return doRequest(username, 'events'); },
+   // };
+
+
+
+  }]);
